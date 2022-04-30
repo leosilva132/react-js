89844bbb272ee703db756be55485ec01ae604b2a
Leandro Viana <leandroviana.vs@gmail.com>
2022-04-30 16:58:13 -0300

    fixing babel configs

---
+++ dist/bundle.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _user$addres;
+
+var user = {
+  name: 'Leandro'
+};
+console.log((_user$addres = user.addres) === null || _user$addres === void 0 ? void 0 : _user$addres.street);
