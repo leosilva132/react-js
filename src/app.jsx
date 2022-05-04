df6769988767c668c074c69f19cecbfe669e8705
unknown <leandroviana.vs@gmail.com>
2022-05-04 14:38:55 -0300

    config dev local

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,5 @@
 export function App(){
+    throw new Error("Eita lasqueira, deu erro bem aqui!")
+
     return <h1>Hello World</h1>
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path')
+const HtmlWebpackplugin = require('html-webpack-plugin')
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-    mode: 'development',
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -10,6 +14,14 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx'],
     },
+    devServer: {
+        static: path.resolve(__dirname, 'public'),
+    },
+    plugins: [
+        new HtmlWebpackplugin({
+            template: path.resolve(__dirname, 'public', 'index.html'),
+        })
+    ],
     module:{
         rules: [
             {
