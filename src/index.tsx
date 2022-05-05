b9fd03cde1fa81bc4c35d8e313b3a1cb612c331b
unknown <leandroviana.vs@gmail.com>
2022-05-05 13:57:56 -0300

    finalizando primeira parte com implementacoes de typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
-import { render } from "react-dom";
+import { createRoot } from 'react-dom/client';
 
 import { App } from "./app"
 
-render(<App />, document.getElementById('root'))
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
+
+// render(<App />, document.getElementById('root'))
 
